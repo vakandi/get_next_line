c8329562a9438de6eddfbbb43997a8c71e9c44bf
wbousfi <wael.bousfira@protonmail.com>
2022-10-30 23:56:56 +0100

    update17152

--- .old/get_next_line.h
+++ .old/get_next_line.h
@@ -3,30 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
+/*   By: wbousfir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/12 02:38:52 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/16 18:19:20 by majjig           ###   ########.fr       */
+/*   Created: 2022/10/11 22:43:59 by wbousfir          #+#    #+#             */
+/*   Updated: 2022/10/13 02:43:41 by wbousfir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-/* INCLUDES */
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
 # include <fcntl.h>
-# include <stdio.h>
-# include <string.h>
 
-/* PROTOTYPES */
+//# define BUFFER_SIZE 150
+
+void	ft_putstr(char *s);
+void	ft_putnbr(int n);
+int		ft_strlen(const char *str);
+void	ft_putstr_fd(int fd, char *str);
+void	ft_putchar(char c);
+void	ft_putchar_fd(int fd, char c);
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2);
-int		is_nl(char *s);
-char	*ft_strdup(char *src);
-int		ft_strlen(char *str);
-char	*new_line(char *line);
-char	*new_remain(char *line, char *remain);
-char	*read_file(int fd, int *end);
+char	*ft_print_line(char *str, char *buf, int j, int i);
+void	ft_free_buffer(char *buf, char *str);
+char	*ft_get_line(char *str, char *buf, int i, int j, int fd);
+
 #endif
